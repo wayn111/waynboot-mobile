11bb41e796ab968c168ea52375ce7ec22d3e127e
waynaqua <1669738430@qq.com>
2020-08-05 20:39:42 +0800

    晚上购物车代码

--- src/views/detail/modules/Tabbar.vue
+++ src/views/detail/modules/Tabbar.vue
@@ -2,7 +2,7 @@
   <div class="tab-bar">
     <van-goods-action>
       <van-goods-action-icon icon="chat-o" text="客服" />
-      <van-goods-action-icon icon="cart-o" text="购物车" :badge="count" />
+      <van-goods-action-icon icon="cart-o" text="购物车" @click="cartClick" :badge="count" />
       <van-goods-action-button type="warning" text="加入购物车" @click="handleClick" />
       <van-goods-action-button :color="variables.red" text="立即购买" @click="handleClick" />
     </van-goods-action>
@@ -37,6 +37,9 @@ export default {
     },
     handleClick() {
       this.$emit('input', true)
+    },
+    cartClick() {
+      this.$router.push({ name: 'Cart' })
     }
 
   }
